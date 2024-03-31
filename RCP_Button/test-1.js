ebbfbfd48c8f8ed9254c8a63aaeb6caf295932bd
Heestolee <dlckdgml25@gmail.com>
2024-04-01 08:41:36 +0900

    가위바위보 새로 push 하는 test

---
+++ RCP_Button/test-1.js
@@ -0,0 +1,57 @@
+
+let com = Math.ceil(Math.random() * 3);
+
+    switch (com) {
+        case 1:
+            com = "가위" ;
+            break;
+        case 2:
+            com = "바위" ;
+            break;
+        case 3:
+            com = "보" ;
+            break;
+    }
+
+
+    console.log(com);
+
+function run(user) {
+    switch (user) {
+        case 'scissors' : {
+            if ( com === '가위') {
+                document.write("컴퓨터 : 가위, 사용자와 비겼습니다.")
+            } else if ( com === '바위') {
+                document.write("컴퓨터 : 바위, 사용자가 졌습니다.")
+            } else if ( com === '보') {
+                document.write("컴퓨터 : 보, 사용자가 이겼습니다.")
+            }
+        break;
+        }
+    
+        case 'rock' : {
+            if ( com == '가위') {
+                document.write("컴퓨터 : 가위, 사용자가 이겼습니다.")
+            } else if ( com == '바위') {
+                document.write("컴퓨터 : 바위, 사용자와 비겼습니다.")
+            } else if ( com == '보') {
+                document.write("컴퓨터 : 보, 사용자가 졌습니다.")
+            }
+        break;
+        }
+    
+        case 'paper' : {
+            if ( com == '가위') {
+                document.write("컴퓨터 : 가위, 사용자가 졌습니다.")
+            } else if ( com == '바위') {
+                document.write("컴퓨터 : 바위, 사용자가 이겼습니다.")
+            } else if ( com == '보') {
+                document.write("컴퓨터 : 보, 사용자와 비겼습니다.")
+            }
+        break;
+        }
+    }
+}
+    // git push test 중 1
+    // git push test 중 2
+        
